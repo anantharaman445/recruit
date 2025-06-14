8e94321aebfe664df26c09de0412c14d1e92f3ba
anantharaman445 <anantharaman45@gmail.com>
2025-06-14 10:00:24 +0530

    sms sender

---
+++ recruit/platform/send_sms.py
@@ -0,0 +1,27 @@
+from twilio.rest import Client
+import os
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+class SendSmsFromTwilio:
+    def __init__(self, message_body, to_phone_number):
+        self.message_body = message_body
+        self.to_phone_number = to_phone_number
+
+    def execute(self):
+        success = True
+        try:
+            message = client.messages.create(
+                body=self.message_body,
+                from_=TWILIO_PHONE_NUMBER,
+                to=self.to_phone_number,
+            )
+        except Exception as e:
+            success = False
+            message = None
+        return success, message
