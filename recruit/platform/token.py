fe8c75621dec0d7dc7288bfd99c68e1fa9261be7
anantharaman445 <anantharaman45@gmail.com>
2025-07-10 23:54:48 +0530

    token removed

--- recruit/platform/token.py
+++ recruit/platform/token.py
@@ -3,11 +3,7 @@ from twilio.jwt.access_token.grants import VideoGrant
 
 import os
 
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
-TWILIO_API_KEY="SK7fd6a271dbcabc4ba7227a68b3314b79"
-TWILIO_API_SECRET="J02dzhl3k8kdAE8c9NX1ueXW9zvcerU1"
+
 
 
 class GenerateTwtilioAccessToken:
