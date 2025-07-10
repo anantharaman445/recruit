8b7aea49c87d1a6d150aecbea1172f679bbd9382
anantharaman445 <anantharaman45@gmail.com>
2025-07-10 23:54:22 +0530

    token changed

--- recruit/platform/token.py
+++ recruit/platform/token.py
@@ -6,13 +6,13 @@ import os
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
-TWILIO_API_KEY=os.getenv("TWILIO_API_KEY")
-TWILIO_API_SECRET=os.getenv("TWILIO_API_SECRET")
+TWILIO_API_KEY="SK7fd6a271dbcabc4ba7227a68b3314b79"
+TWILIO_API_SECRET="J02dzhl3k8kdAE8c9NX1ueXW9zvcerU1"
 
 
-class GenerateTokenForVideo:
+class GenerateTwtilioAccessToken:
     @staticmethod
-    def generate_token(phone_number, room_id):
+    def generate_video_call_token(phone_number, room_id):
         token = AccessToken(
             TWILIO_ACCOUNT_SID,
             TWILIO_API_KEY,  # You'll need to create this in Twilio Console
